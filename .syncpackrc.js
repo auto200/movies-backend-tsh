660fa0d218f06d193c3d740c656dc6168c14223e
auto200 <michal.warac@gmail.com>
2023-09-19 20:56:01 +0200

    chore: pin @types/node in syncpack, fix lint

--- .syncpackrc.js
+++ .syncpackrc.js
@@ -2,6 +2,14 @@
 /** @type {import("syncpack").RcFile} */
 const config = {
   dependencyTypes: ['!local'],
+  versionGroups: [
+    {
+      label: 'Types for specific node version that project uses',
+      packages: ['**'],
+      dependencies: ['@types/node'],
+      pinVersion: '18.11.9',
+    },
+  ],
 };
 
 module.exports = config;
