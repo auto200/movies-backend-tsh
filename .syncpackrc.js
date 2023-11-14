b1449808ed16c7ba46d6ed75f88e92d3b1cb5cbd
auto200 <michal.warac@gmail.com>
2023-11-14 15:46:04 +0100

    chore: bump deps, minimum node version 20

--- .syncpackrc.js
+++ .syncpackrc.js
@@ -2,14 +2,6 @@
 /** @type {import("syncpack").RcFile} */
 const config = {
   dependencyTypes: ['!local'],
-  versionGroups: [
-    {
-      label: 'Types for specific node version that project uses',
-      packages: ['**'],
-      dependencies: ['@types/node'],
-      pinVersion: '18.11.9',
-    },
-  ],
 };
 
 module.exports = config;
