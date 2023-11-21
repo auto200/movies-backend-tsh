a662bb9680c6d5ab215f0306309c2148f7b7b6b5
auto200 <michal.warac@gmail.com>
2023-11-21 14:36:36 +0100

    chore: update deps

--- .syncpackrc.js
+++ .syncpackrc.js
@@ -2,6 +2,15 @@
 /** @type {import("syncpack").RcFile} */
 const config = {
   dependencyTypes: ['!local'],
+  versionGroups: [
+    {
+      // https://github.com/TypeStrong/ts-node/issues/2076
+      label: 'Typescript > 5.2 does not work with ts-node yet',
+      packages: ['**'],
+      dependencies: ['typescript'],
+      pinVersion: '5.2.2',
+    },
+  ],
 };
 
 module.exports = config;
