9959e06f61a56af4b5edaa329c5773857ef11253
Michał Warać <49625375+auto200@users.noreply.github.com>
2023-12-11 04:37:16 +0100

    chore: update deps (#23)

--- .syncpackrc.js
+++ .syncpackrc.js
@@ -2,15 +2,7 @@
 /** @type {import("syncpack").RcFile} */
 const config = {
   dependencyTypes: ['!local'],
-  versionGroups: [
-    {
-      // https://github.com/TypeStrong/ts-node/issues/2076
-      label: 'Typescript > 5.2 does not work with ts-node yet',
-      packages: ['**'],
-      dependencies: ['typescript'],
-      pinVersion: '5.2.2',
-    },
-  ],
+  versionGroups: [],
 };
 
 module.exports = config;
